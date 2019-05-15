038ec106e066de65681ed383c267bbb503140312
msalves008 <matheusdesousaalves2018@gmail.com>
2019-05-15 13:00:20 -0300

    Criação da classe usuario, modificação nas classes professor e aluno

--- BoletimVirtual/src/Model/bean/Professor.java
+++ BoletimVirtual/src/Model/bean/Professor.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model.bean;
 
 /**
@@ -45,9 +41,6 @@ public class Professor extends Usuario {
     public void setTitulacao(String titulacao) {
         this.titulacao = titulacao;
     }
-
-
-  
     
     //Métodos
     public void Imprimir(){
