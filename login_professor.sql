342d59d5836bdc711ca7b2ddb95dbc7298d4dda4
msalves008 <48848963+msalves008@users.noreply.github.com>
2019-05-16 13:04:47 -0300

    Tabelas banco de dados
    adicionando tabelas do banco de dados  ao projeto

---
+++ login_professor.sql
@@ -0,0 +1,2 @@
+INSERT INTO `login_professor` (`id`, `login`, `senha`) VALUES (1, 'master', '123');
+INSERT INTO `login_professor` (`id`, `login`, `senha`) VALUES (2, 'paulocesar', '123');
---
+++ usuario.sql
@@ -0,0 +1,2 @@
+INSERT INTO `usuario` (`id`, `nomeUsuario`, `senha`) VALUES (1, 'matheus', 123);
+INSERT INTO `usuario` (`id`, `nomeUsuario`, `senha`) VALUES (2, 'adm', 123);
