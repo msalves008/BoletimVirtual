c00548a6374e63c3017daba6cf2b2c5c5d0ac528
MATHEUS1 <MATHEUS1@192.168.1.3>
2019-05-18 11:38:26 -0300

    Alteração Clase Aluno novos atributos

--- BoletimVirtual/src/Model/bean/Aluno.java
+++ BoletimVirtual/src/Model/bean/Aluno.java
@@ -1,14 +1,47 @@
 package Model.bean;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matheus
  */
 public class Aluno  extends Usuario{
 
+    //Atributos
     private String  nomeDaMae ,nomeDoPai, telefone, celular ; 
     private int matricula;
-   
+    private Curso curso;
+    private ArrayList<Disciplina> disciplinas;
+    private ArrayList<Nota> notas;
+    
+    //Costrutor PAdrão
+    public Aluno(){
+        super();
+        nomeDaMae = "";
+        nomeDoPai = "";
+        telefone = "";
+        celular = "";
+        matricula = 0;
+        curso = new Curso();
+        disciplinas = new ArrayList();
+        notas = new ArrayList();
+    }
+    
+    //Contrutor Personalizado
+
+    public Aluno(String nomeDaMae, String nomeDoPai, String telefone, String celular, int matricula, Curso curso, ArrayList<Disciplina> disciplinas, ArrayList<Nota> notas) {
+        this.nomeDaMae = nomeDaMae;
+        this.nomeDoPai = nomeDoPai;
+        this.telefone = telefone;
+        this.celular = celular;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.disciplinas = disciplinas;
+        this.notas = notas;
+    }
+    
+    //Getters e Setters
     public int getMatricula() {
         return matricula;
     }
@@ -49,5 +82,28 @@ public class Aluno  extends Usuario{
         this.celular = celular;
     }
 
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(ArrayList<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public ArrayList<Nota> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(ArrayList<Nota> notas) {
+        this.notas = notas;
+    }
     
 }
